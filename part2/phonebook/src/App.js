f91d3f060b43eca53263268816ec3925293d12d4
leowebdev20 <serlebeiso@gmail.com>
2022-08-14 22:06:04 +0200

    useEffect

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,28 +1,54 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Server from "./Server";
+import axios from "axios";
 
 const Person = ({ person }) => {
+  // console.log(person);
+  return (
+    <>
+      <div>
+        <h3>{person.name.common}</h3>
+        <h5>{person.capital}</h5>
+      </div>
+      <div>
+        <h5>{person.area}</h5>
+        <img src={person.flags.png} alt="" />
+      </div>
+    </>
+  );
+};
+const Persons = ({ person }) => {
+  // console.log(person);
   return (
     <div>
-      <h3>
-        {person.name} - {person.number}
-      </h3>
+      <h3>{person.name.common}</h3>
     </div>
   );
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("Add name");
   const [newNumber, setNewNumber] = useState("Add Number");
-  const [search, setSearch] = useState("Name...");
+  const [search, setSearch] = useState("");
   const [peopleToShow, setPeopleToShow] = useState(persons);
 
-  // const peopleToShow = persons.filter((person) => person.name === true);
+  useEffect(() => {
+    console.log("effect");
+    axios.get("https://restcountries.com/v3.1/all").then((response) => {
+      console.log("promise fulfilled", response.data);
+      setPeopleToShow(
+        response.data.filter((person) => {
+          return (
+            person.name.common.toLowerCase().indexOf(search.toLowerCase()) >= 0
+          );
+        })
+      );
+      // setPeopleToShow(response.data);
+      // setPersons(response.data);
+    });
+  }, [search]);
+  console.log("render", persons.length, "persons");
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -50,48 +76,159 @@ const App = () => {
     if (search) {
       setPeopleToShow(
         persons.filter((person) => {
-          return person.name.toLowerCase().indexOf(search.toLowerCase()) >= 0;
+          return (
+            person.name.common.toLowerCase().indexOf(search.toLowerCase()) >= 0
+          );
         })
       );
       setSearch("");
+    } else {
+      setPeopleToShow(persons.filter(() => true));
     }
   };
-  const handlePersonChange = (event) => {
-    console.log(event.target.value);
-    setNewName(event.target.value);
-  };
-  const handleNumChange = (event) => {
-    console.log(event.target.value);
-    setNewNumber(event.target.value);
-  };
+  // const handlePersonChange = (event) => {
+  //   setNewName(event.target.value);
+  // };
+  // const handleNumChange = (event) => {
+  //   setNewNumber(event.target.value);
+  // };
   const handleSearchChange = (event) => {
-    console.log(event.target.value);
     setSearch(event.target.value);
   };
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h2>Countries</h2>
+      <Server />
       <form onSubmit={mySearch}>
         <div>
           SEARCH: <input value={search} onChange={handleSearchChange} />
           <button type="submit">add</button>
         </div>
       </form>
-      <form onSubmit={addPerson}>
+      {/* <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handlePersonChange} />
           number: <input value={newNumber} onChange={handleNumChange} />
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
+      <h2>Numbers</h2> */}
       <ul>
-        {peopleToShow.map((person) => (
-          <Person key={person.name} person={person} />
-        ))}
+        {peopleToShow.length > 10
+          ? "Too many matches!"
+          : peopleToShow.length > 1
+          ? peopleToShow.map((person) => (
+              <Persons key={person.name.common} person={person} />
+            ))
+          : peopleToShow.map((person) => (
+              <Person key={person.name.common} person={person} />
+            ))}
       </ul>
     </div>
   );
 };
 
 export default App;
+
+// import { useState, useEffect } from "react";
+// import Server from "./Server";
+// import axios from "axios";
+
+// const Person = ({ person }) => {
+//   return (
+//     <div>
+//       <h3>
+//         {person.name} - {person.number}
+//       </h3>
+//     </div>
+//   );
+// };
+
+// const App = () => {
+//   const [persons, setPersons] = useState([]);
+//   const [newName, setNewName] = useState("Add name");
+//   const [newNumber, setNewNumber] = useState("Add Number");
+//   const [search, setSearch] = useState("Name...");
+//   const [peopleToShow, setPeopleToShow] = useState(persons);
+
+//   useEffect(() => {
+//     console.log("effect");
+//     axios.get("http://localhost:3001/persons").then((response) => {
+//       console.log("promise fulfilled");
+//       setPeopleToShow(response.data);
+//       setPersons(response.data);
+//     });
+//   }, []);
+//   console.log("render", persons.length, "persons");
+
+//   const addPerson = (event) => {
+//     event.preventDefault();
+//     const personObject = {
+//       name: newName,
+//       number: newNumber,
+//       id: persons.length + 1,
+//     };
+//     if (newName) {
+//       persons.forEach((person) => {
+//         if (JSON.stringify(personObject) === JSON.stringify(person)) {
+//           alert(`${personObject.name} name already inserted`);
+//         } else {
+//           setPeopleToShow(persons.concat(personObject));
+//           setPersons(persons.concat(personObject));
+//           setNewName("");
+//           setNewNumber("");
+//           setSearch("");
+//         }
+//       });
+//     }
+//   };
+//   const mySearch = (event) => {
+//     event.preventDefault();
+//     if (search) {
+//       setPeopleToShow(
+//         persons.filter((person) => {
+//           return person.name.toLowerCase().indexOf(search.toLowerCase()) >= 0;
+//         })
+//       );
+//       setSearch("");
+//     } else {
+//       setPeopleToShow(persons.filter(() => true));
+//     }
+//   };
+//   const handlePersonChange = (event) => {
+//     setNewName(event.target.value);
+//   };
+//   const handleNumChange = (event) => {
+//     setNewNumber(event.target.value);
+//   };
+//   const handleSearchChange = (event) => {
+//     setSearch(event.target.value);
+//   };
+//   return (
+//     <div>
+//       <h2>Phonebook</h2>
+//       <Server />
+//       <form onSubmit={mySearch}>
+//         <div>
+//           SEARCH: <input value={search} onChange={handleSearchChange} />
+//           <button type="submit">add</button>
+//         </div>
+//       </form>
+//       <form onSubmit={addPerson}>
+//         <div>
+//           name: <input value={newName} onChange={handlePersonChange} />
+//           number: <input value={newNumber} onChange={handleNumChange} />
+//           <button type="submit">add</button>
+//         </div>
+//       </form>
+//       <h2>Numbers</h2>
+//       <ul>
+//         {peopleToShow.map((person) => (
+//           <Person key={person.name} person={person} />
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
+
+// export default App;
---
+++ part2/phonebook/src/Server.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const Server = ({ course }) => {
+  // console.log(course);
+  return (
+    <>
+      <div>Hello</div>
+    </>
+  );
+};
+
+export default Server;
