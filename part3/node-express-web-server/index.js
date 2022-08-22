31bbeab4106083488fa1aca424bb44a772339891
leowebdev20 <serlebeiso@gmail.com>
2022-08-22 23:47:15 +0200

    web-server

---
+++ part3/node-express-web-server/index.js
@@ -0,0 +1,96 @@
+const express = require("express");
+var morgan = require("morgan");
+
+var app = express();
+
+app.use(express.json());
+
+app.get("/", function (req, res) {
+  //   console.log('Method:', req.method)
+  //   console.log('Path:  ', req.path)
+  //   console.log('Body:  ', req.body)
+  res.send("hello, world!");
+});
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :req[content] :res[content]"
+  )
+);
+
+const generateId = () => {
+  return Math.floor(Math.random() * 1000);
+};
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
+});
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  if (!body.name) {
+    return response.status(400).json({
+      error: "name missing",
+    });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number || "0",
+    id: generateId(),
+  };
+
+  persons = persons.concat(person);
+
+  response.json(person);
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
