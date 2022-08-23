f2a21bb26273e5c44bf5c35c48006586bbc99bfd
leowebdev20 <serlebeiso@gmail.com>
2022-08-23 19:12:52 +0200

    fix

--- part3/node-express-web-server/index.js
+++ part3/node-express-web-server/index.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 var app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static('build'))
 
 app.get("/", function (req, res) {
   //   console.log('Method:', req.method)
